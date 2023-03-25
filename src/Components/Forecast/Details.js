1749568192e4b06ed4aae6d7a06a03d9daad3ebd
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-03-25 14:44:16 +0100

    I don't know what is going on anymore

---
+++ src/Components/Forecast/Details.js
@@ -0,0 +1,10 @@
+export default function Details(props) {
+    return (
+        <div>
+            <div>
+                <h3 className="parameter-row">45</h3>
+                <span>Humidity</span>
+            </div>
+        </div>
+    )
+}
--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -1,9 +1,19 @@
 import '../Forecast/Forecast.css';
+import Details from './Details';
 
 export default function Forecast() {
     return (
         <div className='container'>
-            <h1>Hello world</h1>
+            <p>City name</p>
+            <div>weather description</div>
+            <div className='weather-details'>
+                <img className='weather-icon'></img>
+                <h3 className='temp'>45 Degrees</h3>
+            </div>
+           <Details
+    
+           />
+            
         </div>
     )
 }
