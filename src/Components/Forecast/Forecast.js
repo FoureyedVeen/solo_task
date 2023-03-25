1c69c715394af2252b181962258da87bb348536b
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-03-25 15:05:08 +0100

    Feat: parameter-container styling

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -4,16 +4,17 @@ import Details from './Details';
 export default function Forecast() {
     return (
         <div className='container'>
-            <p>City name</p>
-            <div>weather description</div>
-            <div className='weather-details'>
-                <img className='weather-icon'></img>
-                <h3 className='temp'>45 Degrees</h3>
-            </div>
-           <Details
-    
-           />
+            <div className='parameter-box'>
+                <p>City name</p>
+                <div>weather description</div>
+                <div className='weather-details'>
+                    <img className='weather-icon'></img>
+                    <h3 className='temp'>45 Degrees</h3>
+                </div>
+                <Details
             
+                />
+           </div> 
         </div>
     )
 }
