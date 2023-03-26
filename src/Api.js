ed8aaf942adb49873bc4b0f6b5b550b5fe6384fc
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-03-26 23:13:21 +0100

    feat: haandleOnchange function for the search bar.

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,4 @@
-export const options = {
+export const geoApiOptions = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'feb99593b7msh14485d138055071p1904bbjsn0f937632ab3a',
@@ -6,7 +6,9 @@ export const options = {
 	}
 };
 
-fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions', options)
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+fetch('/cities?minPopulation=1000000', geoApiOptions)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiOptions, GEO_API_URL } from "../Api";
 
-export default function SearchBar() {
+
+export default function SearchBar({onSearchChange}) {
     const [search, setSearch] = useState(null)
-     
+    
+    const handleOnchange(searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData)
+    }
+
+
+
     return (
        
         <div className="search-bar">
@@ -12,6 +21,7 @@ export default function SearchBar() {
                 className="input"
                 debounceTimeout={800}
                 value = {search}
+                onChange = {handleOnchange}
              />
         </div>
     )
