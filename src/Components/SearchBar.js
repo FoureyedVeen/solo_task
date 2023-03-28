b8633214f92855c6a3867792ce36ac04a324cf6c
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-03-28 15:04:26 +0100

    new package Async Select

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,6 +1,8 @@
-import { async } from "q";
+// import { response } from "express";
 import React, { useState } from "react";
-import { AsyncPaginate } from "react-select-async-paginate";
+import AsyncSelect from 'react-select/async';
+// import { AsyncPaginate } from "react-select-async-paginate";
+
 import { geoApiOptions, GEO_API_URL } from "../Api";
 
 
@@ -9,13 +11,33 @@ export default function SearchBar({onSearchChange}) {
     
     async function loadOptions(inputValue) {
         console.log(inputValue)
-        fetch (
+        // const data = fetch (
+        //         `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, 
+        //         {...geoApiOptions}
+        // )
+        // .then(response => response.json())
+        // .then(response => response.data)
+        // .catch(err => console.error(err));
+        const response = await fetch (
                 `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, 
                 geoApiOptions
         )
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+        const data = await response.json();
+        const newData = data?.data?.map(element => {
+        
+            return {
+                value: element.city,
+                label: element.city
+                
+            }
+
+         });
+         console.log(newData)
+         return [];
+        //  return {
+        //     options: newData || [],
+        //     hasMore : true
+        //  }
             // return {
             //     options:response.data.map((city)) => {
             //         value: `${data.city.latitude} ${data.city.longitude}`,
@@ -28,20 +50,26 @@ export default function SearchBar({onSearchChange}) {
 
 
     function handleOnchange(searchData) {
-        setSearch(searchData);
+        
         onSearchChange(searchData);
     };
 
     return (
        
         <div className="search-bar">
-             <AsyncPaginate 
+             <AsyncSelect
                 placeholder = "Search for city"
                 className="input"
                 debounceTimeout={800}
                 value = {search}
                 onChange = {handleOnchange}
-                loadOptions = {loadOptions}
+                loadOptions = {(inputValue, callBack) => {
+                    setTimeout(() => {
+                        callBack(loadOptions(inputValue));
+                      }, 1000);
+                    }
+                    // callBack(s(inputValue) || [])}}
+                }
              />
         </div>
     )
