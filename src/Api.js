1a4489a62aca6ed70236de4fc865dbed8502e974
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-04-17 21:07:23 +0100

    Feat: Forecast Api

--- src/Api.js
+++ src/Api.js
@@ -8,3 +8,16 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
 
+// Forecast Api
+export const ForecastApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'feb99593b7msh14485d138055071p1904bbjsn0f937632ab3a',
+		'X-RapidAPI-Host': 'forecast9.p.rapidapi.com'
+	}
+};
+
+fetch('https://forecast9.p.rapidapi.com/rapidapi/forecast/Berlin/summary/', ForecastApiOptions)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
