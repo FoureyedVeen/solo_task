f5578b7cc66af88c5c61137162ce66b0e755d45e
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-04-17 21:06:33 +0100

    Feat: Current date updated

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -1,15 +1,17 @@
 import '../Forecast/Forecast.css';
 import Details from './Details';
 import cloudIcon from '../../images/cloudIcon.png'
-
+import SearchBar from '../SearchBar';
 
 export default function Forecast() {
+    const current = new Date();
+    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
     return (
         <div className='container'>
             <div className='parameter-box'>
                 <div className='row--1'>
-                    <span id='city-name'>London</span>
-                    <span>23/03/2023</span>
+                    <span id='city-name'>{}</span>
+                    <span>{date}</span>
                 </div> 
                 {/* <div>weather description</div> */}
                 <div className='weather-details'>
